1b39d78466415d68f7d950be386663718641d610
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2021-08-02 20:36:33 +0800

    Add anonymous posting

--- forms_dir/newtopic_form.py
+++ forms_dir/newtopic_form.py
@@ -1,12 +1,15 @@
 from django import forms
+from django.forms import BooleanField
+
 from boards.models import Topic
 from mdeditor.fields import MDTextFormField
 
 class NewTopic_Form(forms.ModelForm):
 	message = MDTextFormField()
+	anonymous = BooleanField(required=False)
 	class Meta:
 		model = Topic
-		fields = ['subject', 'message']
+		fields = ['subject', 'message', 'anonymous']
 		help_texts = {'subject':'Maximum length is 300'}
 		widgets = {'subject':forms.TextInput(attrs={'placeholder': 'enter subject of topic here'}) }	#model field subject is CharField so produces form field Charfield which has widget as TextInput
 		#subject is of Topic and message(for Post ) declared outside Meta
